898bd2d3bba2041c3a566df2cd888f16b9abf15b
SangKwon-Lee <sangkwon.lee@pxd.co.kr>
2023-09-26 18:24:59 +0900

    fix: color 수정

--- src/components/list/RatingReviewList.tsx
+++ src/components/list/RatingReviewList.tsx
@@ -8,27 +8,31 @@ interface Props {
 
 export default function RatingReviewList({ ratingList }: Props) {
   return (
-    <RatingList>
+    <>
       <RatingSub>최근 10개의 별점만 표시됩니다</RatingSub>
-      {Array.isArray(ratingList) && ratingList.length > 0 ? (
-        ratingList.map((rating, index) => (
-          <RatingItem key={index}>
-            <Review
-              rating={rating.attributes.rating}
-              date={rating.attributes.updatedAt}
-              nickname={rating.attributes.user.data?.attributes.nickname}
-            />
-          </RatingItem>
-        ))
-      ) : (
-        <RatingDate>아직 리뷰가 없습니다.</RatingDate>
-      )}
-    </RatingList>
+      <RatingList>
+        {Array.isArray(ratingList) && ratingList.length > 0 ? (
+          ratingList.map((rating, index) => (
+            <RatingItem key={index}>
+              <Review
+                rating={rating.attributes.rating}
+                date={rating.attributes.updatedAt}
+                nickname={rating.attributes.user.data?.attributes.nickname}
+              />
+            </RatingItem>
+          ))
+        ) : (
+          <RatingDate>아직 리뷰가 없습니다.</RatingDate>
+        )}
+      </RatingList>
+    </>
   );
 }
 
 const RatingSub = styled.div`
   color: ${({ theme }) => theme.gray.gray30};
+  padding: 20px 0;
+  border-top: 1px solid ${({ theme }) => theme.gray.gray80};
   ${({ theme }) => theme.textSize.S14W400};
 `;
 
@@ -36,9 +40,7 @@ const RatingList = styled.ul`
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding-top: 20px;
   padding-bottom: 20px;
-  border-top: 1px solid ${({ theme }) => theme.gray.gray80};
   border-bottom: 1px solid ${({ theme }) => theme.gray.gray80};
   gap: 24px;
 `;
@@ -53,7 +55,7 @@ const RatingItem = styled.li`
   }
 `;
 
-const RatingDate = styled.div`
+const RatingDate = styled.li`
   padding-top: 2px;
   color: ${({ theme }) => theme.gray.gray40};
   ${({ theme }) => theme.textSize.S14W400};
